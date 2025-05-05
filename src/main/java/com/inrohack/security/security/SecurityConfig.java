76ef567be6d1f6ae3503df93464fabc52d457265
peterParker79 <trypeterparker@gmail.com>
2025-05-05 15:12:39 +0200

    SecurityConfig Class Basic auth

--- src/main/java/com/inrohack/security/security/SecurityConfig.java
+++ src/main/java/com/inrohack/security/security/SecurityConfig.java
@@ -1,7 +1,31 @@
 package com.inrohack.security.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+
+
 public class SecurityConfig {
+
+    @Bean
+    // Se definen la cadena de filtros de seguridad
+    // HttpSecurity -> objeto que permite configurar la seguridad HTTP
+    public SecurityFilterChain filterChain(HttpSecurity http)    throws Exception {
+        http
+                // aqui indicamos que rutas tienen acceso sin autenticación y con auth.
+                .authorizeHttpRequests( authz ->authz
+                        .requestMatchers("public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                // El tipo de autenticación: básica http
+                .httpBasic(Customizer.withDefaults());
+
+        //aquí devolvemos la configuración completa.
+        return http.build();
+    }
+
 }
